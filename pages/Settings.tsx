d067f586636351c791f7ac4a6d8821b16f6ea2a4
Thales Valentini <ext.tvalentini@riotgames.com>
2023-09-24 01:00:51 -0300

    Settings page

--- pages/Settings.tsx
+++ pages/Settings.tsx
@@ -1,12 +1,44 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { ScrollView, View } from "react-native";
 import { styles } from "../styles/settingsStyles";
+import { SafeAreaView } from "react-navigation";
+import { Avatar, Button, Divider, List, Text } from "react-native-paper";
 
 export default function Settings() {
   return (
-    <View style={styles.container}>
-      <Text>Settings</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.body}>
+          <View style={styles.avatar}>
+            <Avatar.Icon icon="account-outline" size={70} />
+            <Text style={styles.wallet} variant="titleMedium">
+              0x449...F52B
+            </Text>
+            <Button mode="text">Logout</Button>
+          </View>
+          <Divider />
+
+          <List.Section>
+            <List.Subheader>Preferences</List.Subheader>
+            <List.Item title="Theme" />
+            <List.Item title="Languages" />
+            <List.Item title="Moods" />
+          </List.Section>
+          <Divider />
+          <List.Section>
+            <List.Subheader>Security</List.Subheader>
+            <List.Item title="App Lock" />
+            <List.Item title="Therapy Share" />
+          </List.Section>
+          <Divider />
+          <List.Section>
+            <List.Subheader>Entries</List.Subheader>
+            <List.Item title="Save Local Backup" />
+            <List.Item title="Reset Entries" />
+          </List.Section>
+
+          <Button mode="contained">BUY JOURNALY</Button>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
--- styles/settingsStyles.ts
+++ styles/settingsStyles.ts
@@ -3,8 +3,19 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: "#fffbfe",
+  },
+  body: {
+    marginTop: 60,
+    marginHorizontal: 15,
+    gap: 15,
+    paddingBottom: 15,
+  },
+  wallet: {
+    marginTop: 15,
+  },
+  avatar: {
     justifyContent: "center",
+    alignItems: "center",
   },
 });
